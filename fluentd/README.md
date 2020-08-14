159606125f3c8addedaa14731b732283325b4e7d
Wang Xing Yang <wxy@rowenatech.com>
2020-08-14 20:43:30 +0800

    add fluentd

---
+++ fluentd/README.md
@@ -0,0 +1,8 @@
+使用说明
+=============
+
+worker: 定时写日志
+
+fluentd1: 读日志文件，转发到fluentd2
+
+fluentd2: 接收日志
---
+++ fluentd/worker.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+
+
+cd /data
+
+while true
+do
+  echo "1" >> /data/access.log 
+
+  sleep 1
+
+done
